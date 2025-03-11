a13fa9f8589b710a88da822a204443873c22f868
santhosh3825 <146931033+santhosh3825@users.noreply.github.com>
2025-03-11 11:55:15 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://hemarangaswamy0411:Hema@cluster0.zl3v7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial qx  
